36746da1bfdeae9e7ff0af5df1bcc11ff45b54cf
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-08 14:39:22 +0530

    how to add choices in models

--- core/models.py
+++ core/models.py
@@ -4,11 +4,23 @@ from django.contrib.auth.models import User
 
 # Create your modmels here.
 class Restaurant(models.Model):
+    class Typechoices(models.TextChoices):
+
+        INDIAN = 'IN', 'Indian'
+        CHINESE = 'CH', 'Chinese'
+        ITALIAN = 'IT', 'Italian'
+        GREEK = 'GR', 'Greek'
+        MEXICAN = 'MX', 'Mexican'
+        FASTFOOD = 'FF', 'fast Food'
+        OTHER = 'OT', 'Other'
+
+
     name = models.CharField(max_length=100)
     website = models.URLField(default='')
     date_opened = models.DateField()
     latitude = models.FloatField()
     longitude = models.FloatField()
+    restaurant_type = models.CharField(max_length=2,choices=Typechoices.choices)
 
     def __init__(self):
         return self.name
@@ -21,3 +33,4 @@ class Rating(models.Model):
 
     def __str__(self):
         return f"Rating:{self.rating}"
+
