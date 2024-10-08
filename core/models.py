a1de39512bff30a566d75b3b7fd035fe1fb879f9
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-08 14:05:34 +0530

    models and migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+# Create your modmels here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(default='')
+    date_opened = models.DateField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __init__(self):
+        return self.name
