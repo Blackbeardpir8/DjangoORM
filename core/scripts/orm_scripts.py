79fb2eef242d32eb006262d6e207d46588ed0dc6
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-09 16:23:00 +0530

    getting data from database

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -2,14 +2,6 @@ from core.models import Restaurant
 from django.utils import timezone
 
 def run():
-
-    restaurant = Restaurant()
-    restaurant.name = "THE ITALIAN PASTA"
-    restaurant.website = "theitalianpasta@gmail.com"
-    restaurant.latitude = 55.34
-    restaurant.longitude = 87.23
-    restaurant.date_opened = timezone.now()
-    restaurant.restaurant_type = Restaurant.Typechoices.ITALIAN
-
-    restaurant.save()
+    restaurants = Restaurant.objects.all()
+    print(restaurants)
 
