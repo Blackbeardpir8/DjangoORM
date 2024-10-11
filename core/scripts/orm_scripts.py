8b4cfec536137910fc6c981ab8f0a9f46d5a37a4
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-11 12:16:00 +0530

    other way to create records

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -2,6 +2,11 @@ from core.models import Restaurant
 from django.utils import timezone
 
 def run():
-    restaurants = Restaurant.objects.all()
-    print(restaurants)
-
+    Restaurant.objects.create(
+        name = "THE INDIAN IDLI",
+        date_opened = timezone.now(),
+        restaurant_type = Restaurant.Typechoices.INDIAN,
+        website = "theindialidli@gmail.com",
+        latitude = 34.23,
+        longitude = 67.35,
+    )
