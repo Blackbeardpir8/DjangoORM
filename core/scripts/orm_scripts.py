52014eb224656cf53678a5465e82c78a94ee7168
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-12 14:48:18 +0530

    get or create

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -2,9 +2,14 @@ from core.models import Restaurant, Rating, User
 from django.utils import timezone
 
 def run():
-    rating = Rating.objects.first()
-    print(rating.restaurant)
+    user = User.objects.first()
+    restaurant = Restaurant.objects.first()
 
+    print(Rating.objects.get_or_create(
+        restaurant = restaurant,
+        user = user,
+        rating =4
+    ))
 
 
 
