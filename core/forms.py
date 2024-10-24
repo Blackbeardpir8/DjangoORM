241720896cdab849795008b600440c4b33f807b8
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-24 10:22:16 +0530

    update django model instances

--- core/forms.py
+++ core/forms.py
@@ -4,4 +4,4 @@ from core.models import Restaurant
 class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
-        fields = ('name','restaurant_type')
+        fields = ('name','restaurant_type') 
--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -2,11 +2,6 @@ from core.models import Restaurant, Rating, User
 from django.utils import timezone
 
 def run():
-    user = User.objects.first()
     restaurant = Restaurant.objects.first()
-
-    rating=Rating(user = user, restaurant=restaurant , rating = 9)
-
-    rating.full_clean()
-    rating.save()
-
+    restaurant.name = "THE ITALIAN PIZZA PASTA AND RAAIOLI"
+    restaurant.save()
