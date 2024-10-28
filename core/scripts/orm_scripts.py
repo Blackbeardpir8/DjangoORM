320e6338447740d658fdac747b3d37904de045a6
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-28 15:54:49 +0530

    querysets containing records with exists method

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -2,9 +2,9 @@ from core.models import Restaurant, Rating,Sale
 from django.utils import timezone
 
 def run():
-    restaurant = Restaurant.objects.filter(name = "Pizzeria 1")
-    print(restaurant)
-    print(restaurant.get())
+    restaurant = Restaurant.objects.filter(restaurant_type = Restaurant.TypeChoices.ITALIAN)
+    print(restaurant.exists())
+    
     
 
 
