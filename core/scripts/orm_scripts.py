d13ca7ebf51a2c77861b2e5eceec443825b06067
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-28 10:45:22 +0530

    filtering querysets with filter method

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -2,9 +2,7 @@ from core.models import Restaurant, Rating,Sale
 from django.utils import timezone
 
 def run():
-    print(Restaurant.objects.count())
-    print(Rating.objects.count())
-    print(Sale.objects.count())
+    print(Restaurant.objects.filter(restaurant_type= Restaurant.TypeChoices.CHINESE))
     
 
 
