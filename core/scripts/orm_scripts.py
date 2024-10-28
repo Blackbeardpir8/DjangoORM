a1969bc6f7b3626612f43f38e50de68f46e9a2d0
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-28 20:05:53 +0530

    filtering queryset using in method

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -3,9 +3,11 @@ from django.utils import timezone
 
 def run():
     chinese = Restaurant.TypeChoices.CHINESE
-    restaurants = Restaurant.objects.filter(restaurant_type=chinese,name__startswith='C')
+    indian = Restaurant.TypeChoices.INDIAN
+    mexican = Restaurant.TypeChoices.MEXICAN
+    check_types = [chinese,indian,mexican]
+    restaurants = Restaurant.objects.filter(restaurant_type__in=check_types)
     print(restaurants)
-
     
     
 
