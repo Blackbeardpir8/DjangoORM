0a9680d73e19918820583d86322895ea0dd0b849
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-28 11:33:34 +0530

    get queryset and single model

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -2,7 +2,9 @@ from core.models import Restaurant, Rating,Sale
 from django.utils import timezone
 
 def run():
-    print(Restaurant.objects.filter(restaurant_type= Restaurant.TypeChoices.CHINESE))
+    restaurant = Restaurant.objects.filter(name = "Pizzeria 1")
+    print(restaurant)
+    print(restaurant.get())
     
 
 
