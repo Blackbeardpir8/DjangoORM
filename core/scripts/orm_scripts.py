59fe7571f9d4a50a4c5e43f59340fbc741767b6f
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-29 11:22:12 +0530

    filtering queryset using __lt lookup

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -2,10 +2,7 @@ from core.models import Restaurant, Rating,Sale
 from django.utils import timezone
 
 def run():
-    chinese = Restaurant.TypeChoices.CHINESE
-    indian = Restaurant.TypeChoices.INDIAN
-    check_types = [chinese,indian]
-    restaurants = Restaurant.objects.exclude(restaurant_type__in = check_types)
+    restaurants = Restaurant.objects.filter(name__lt = "E")
     print(restaurants)
     
     
