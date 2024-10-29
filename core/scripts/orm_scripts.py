0a6048c46beb00ee097e0c7ea0fc4221848be224
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-29 20:36:35 +0530

    indexing into queryset

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -2,7 +2,7 @@ from core.models import Restaurant, Rating,Sale
 from django.utils import timezone
 from django.db.models.functions import Lower
 def run():
-    restraunts = Restaurant.objects.order_by('-name')
+    restraunts = Restaurant.objects.order_by('date_opened')[0]
     print(restraunts)
 
     
