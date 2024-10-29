06850f5208d035d27473faf747f9471188563b67
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-10-29 11:27:46 +0530

    filtering queryset using __gt lookup

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -2,7 +2,7 @@ from core.models import Restaurant, Rating,Sale
 from django.utils import timezone
 
 def run():
-    restaurants = Restaurant.objects.filter(name__lt = "E")
+    restaurants = Restaurant.objects.filter(name__gt = "E")
     print(restaurants)
     
     
