37dc8b7ca8a7b1f51b04a89aa0013bed359ff94f
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-11-01 10:25:14 +0530

    prefetch_related to avoid N+1 problem

--- core/views.py
+++ core/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
 from core.forms import RestaurantForm
+from core.models import Restaurant,Sale,Rating
 # Create your views here.
 
 def index(request):
-    if request.method == 'POST':
-        form = RestaurantForm(request.POST or None)
-        if form.is_valid():
-            print(form.cleaned_data)
-        else:
-            return render(request, 'index.html', {'form':form})
-        
-    context = {'form':RestaurantForm()}
-    return render(request, 'index.html',context) 
+    restaurants = Restaurant.objects.prefetch_related('ratings')
+    context = {'restaurants': restaurants}
+    return render(request, 'index.html',context)    
