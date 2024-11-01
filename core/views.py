09b1c8ca1314da90288c1bf325225942c10a508f
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-11-01 10:47:45 +0530

    filter and prefetch_related to avoid N+1 problem

--- core/views.py
+++ core/views.py
@@ -4,6 +4,6 @@ from core.models import Restaurant,Sale,Rating
 # Create your views here.
 
 def index(request):
-    restaurants = Restaurant.objects.prefetch_related('ratings','sales')
+    restaurants = Restaurant.objects.filter(name__istartswith='c').prefetch_related('ratings', 'sales')
     context = {'restaurants': restaurants}
     return render(request, 'index.html',context)    
