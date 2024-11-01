507632c4d53c2df493fddb8bab1ab896cec88e09
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-11-01 21:32:14 +0530

    prefetch objects

--- core/views.py
+++ core/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from core.forms import RestaurantForm
 from core.models import Restaurant,Sale,Rating
+from django.db.models import Sum,Prefetch
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
-    ratings = Rating.objects.only('rating','restaurant__name').select_related('restaurant')
-    context = {'ratings':ratings}
-    return render(request, 'index.html',context)     
+    #Get all 5star rating and fetch all the sales for the restaurants with 5 stae ratings
+    month_ago = timezone.now()-timezone.timedelta(days=30)
+    monthly_sales = Prefetch(
+        'sales',
+        queryset=Sale.objects.filter(datetime__gte=month_ago)
+    )
+    restaurants = Restaurant.objects.filter(ratings__rating=5).prefetch_related('ratings', monthly_sales).annotate(total=Sum('sales__income'))
+    restaurants.annotate(total=Sum('sales__income'))
+    print([r.total for r in restaurants])
+    return render(request, 'index.html')     
