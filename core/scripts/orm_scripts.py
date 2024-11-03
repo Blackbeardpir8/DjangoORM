84ca8fad56c609060d8201b2b70ac576bfeb296c
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-11-03 12:49:20 +0530

    remove all assotiations from staff

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.db.models.functions import Lower
 def run():
     staff , created = Staff.objects.get_or_create(name = "John Wick")
-    staff.restaurants.set(Restaurant.objects.all()[:5])
+    staff.restaurants.clear()
     print(staff.restaurants.count())
     
 
