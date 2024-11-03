1473a6e9fb6c4081db570c4743a67644e2f69bfb
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-11-03 12:20:03 +0530

    associating staff to restaurants and check

--- core/scripts/orm_scripts.py
+++ core/scripts/orm_scripts.py
@@ -3,11 +3,9 @@ from django.utils import timezone
 from django.db.models.functions import Lower
 def run():
     staff , created = Staff.objects.get_or_create(name = "John Wick")
-    print(staff)
-    print(type(staff.restaurants))
     print(staff.restaurants.all())
-
-    
+    staff.restaurants.add(Restaurant.objects.first())
+    print(staff.restaurants.all())
     
 
 
